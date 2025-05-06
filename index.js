6585800c465f1ceef3fecd7530f3d0c05f5df674
Tatyana Elhina <tatyana.elkhina@mail.ru>
2025-05-06 11:34:14 +0300

    Ответы клиенту

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ const app = express();
 const port = 8000;
 
 app.get('/hello', (req, res) => {
-	res.send('Привет!');
-})
+//	res.send('Привет!');
+	res.end();
+});
 
 app.listen(port, () => {
 	console.log(`Сервер запущен на http://localhost:${port}`);
