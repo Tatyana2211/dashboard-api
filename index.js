b5b9cba29135229ff62155d57ec78e24b4b1c397
Tatyana Elhina <tatyana.elkhina@mail.ru>
2025-05-20 12:21:15 +0300

    Промежуточные обработчики

--- index.js
+++ index.js
@@ -4,12 +4,22 @@ import { userRouter } from './users/users.js';
 const app = express();
 const port = 8000;
 
+app.use((req, res, next) => {
+	console.log('Время ', Date.now());
+	next();
+});
+
 app.get('/hello', (req, res) => {
-//	res.send('Привет!');
-	res.end();
+	throw new Error('Error!!!');
 });
 
 app.use('/users', userRouter);
+
+
+app.use((err, req, res, next) => {
+	console.log(err.message);
+	res.status(401).send(err.message);
+});
 
 app.listen(port, () => {
 	console.log(`Сервер запущен на http://localhost:${port}`);
